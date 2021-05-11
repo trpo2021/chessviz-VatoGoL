1928670aed3d9d608725758476748fdd8a55e4ce
VatoGoL <78630616+VatoGoL@users.noreply.github.com>
2021-05-11 11:18:14 +0700

    Update tools.h

--- src/libchess/tools.h
+++ src/libchess/tools.h
@@ -1,6 +1,6 @@
 #pragma once
 
 void Print_table(char Table[9][9]);
-void Preparation (char Table_Chess [9][9], unsigned int p);
-FILE *Checking_Resources(FILE *file, char Name_file [30]);
-void Checking_moves(FILE *file, unsigned int p, char Table_chess [9][9]);
+void Preparation(char Table_Chess[9][9], unsigned int p);
+FILE *Checking_Resources(FILE *file, char Name_file[30]);
+void Checking_moves(FILE *file, unsigned int p, char Table_chess[9][9]);
