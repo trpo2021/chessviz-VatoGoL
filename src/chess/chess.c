4d8c5b765d04d1461bf6b5a4fbd94a3e226dc92a
VatoGoL <78630616+VatoGoL@users.noreply.github.com>
2021-05-11 11:20:17 +0700

    Update chess.c

--- src/chess/chess.c
+++ src/chess/chess.c
@@ -3,22 +3,20 @@
 #include <string.h>
 #include <libchess/tools.h>
 
-int main ()
-{
+int main() {
+  unsigned int p = 0;
+  char Table_chess[9][9] = {};
+  //
+  Preparation(Table_chess, p);  // Размещаем фигуры на доске
+  //
+  FILE *file = 0;
+  char Name_file[30];
 
-	unsigned int p = 0;
-	char Table_chess [9][9] = {};
-	//
-	Preparation(Table_chess,p); // Размещаем фигуры на доске
-	//
-	FILE *file = 0;
-	char Name_file[30];
+  file = Checking_Resources(file, Name_file);
+  //Проверяем на месте  ли файл с ходами
 
-	file = Checking_Resources(file,Name_file);
-	//Проверяем на месте  ли файл с ходами
+  Checking_moves(file, p, Table_chess);
 
-	Checking_moves(file,p,Table_chess);
-
-	Print_table(Table_chess);
-	return 0;
+  Print_table(Table_chess);
+  return 0;
 }
